976bbb9fb043804e68b87f8b6eed6ada2bbcf2a2
zhj <1018783112@qq.com>
2019-02-15 20:10:14 +0800

    引入gulp相关插件

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2019-02-15 16:36:21 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2019-02-15 20:09:18
+ */
+
+var gulp = require('gulp'); //载入模块
+var sass = require('gulp-sass'); //编译sass
+var mincss = require('gulp-clean-css'); //压缩css
+var rename = require('gulp-rename'); //文件重命名
+var uglify = require('gulp-uglify'); //压缩文件不支持ES6
+var babel = require('gulp-babel'); //将ES6转为ES5
+var htmlmin = require('gulp-htmlmin'); //压缩html
+var server = require('gulp-webserver'); //开启服务
+var autoprefixter = require('gulp-autoprefixer'); //自动添加浏览器前缀
+var fs = require('fs'); //fs
+var path = require('path'); //
+var url = require('url'); //
+var data = require('./src/mork/data.json'); //
+//建立任务
+//编译sass
+gulp.task('conScss', function() {
+    return gulp.src('./src/style/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest("./src/css"))
+})
+
+gulp.task('auto', function() {
+    return gulp.watch('./src/style/**/*.scss', gulp.series("conScss"))
+})
+
+//压缩css
+gulp.task("zipcss", function() {
+        return gulp.src('./src/style/**/*.scss')
+            .pipe(mincss())
+            .pipe(gulp.dest("./build/css"))
+    })
+    //压缩js
+gulp.task("zipjs", function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest("./build/js"))
+})
--- src/gulpfile.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: mikey.zhaopeng 
- * @Date: 2019-02-15 16:36:21 
- * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-02-15 16:42:58
- */
-
-var gulp = require('gulp');
